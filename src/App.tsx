3f6a62aed14716888733cf6ac272b9872c3720e2
JavaScript Joe <jjprevite@gmail.com>
2019-12-08 18:41:22 -0700

    feat: add example with input

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const [userName, setUserName] = React.useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setUserName(value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label htmlFor="userName">Username:</label>
+        <input
+          id="userName"
+          placeholder="Enter your username here..."
+          value={userName}
+          onChange={handleChange}
+        />
+      </form>
     </div>
   );
 }
